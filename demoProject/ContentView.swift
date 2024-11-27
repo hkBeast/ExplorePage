9b0bc7031c2734a717c5d84b0f9494d0fbf97c6c
hkBeast <himanshu.singh@simplyentertaining.us>
2024-11-27 16:46:27 +0530

    Initial Commit

---
+++ demoProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demoProject
+//
+//  Created by HKBeast on 27/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demoProject/demoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoProjectApp.swift
+//  demoProject
+//
+//  Created by HKBeast on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct demoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
